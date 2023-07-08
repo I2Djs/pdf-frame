b8861b0dd122f81d07781f793dea3263d35b6e6c
Narayana Swamy <narayanaswamy14@gmail.com>
2023-07-08 22:44:47 +0530

    updated package.json

--- packages/pdf-frame-vue/src/main.js
+++ packages/pdf-frame-vue/src/main.js
@@ -3,6 +3,4 @@ import './style.css'
 import App from './App.vue'
 import i2dClient from './../dist/i2d-client.es.js';
 
-console.log(i2dClient);
-
 createApp(App).component("i2dClient", i2dClient).mount('#app');
