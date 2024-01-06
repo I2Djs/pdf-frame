56ad4067550edb5639bde1ddf9026ff6420f3e9c
Narayana Swamy <narayanaswamy14@gmail.com>
2024-01-06 15:35:27 +0530

    Updated i2djs version

--- packages/pdf-frame-vue/src/main.js
+++ packages/pdf-frame-vue/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import './style.css';
-import App from './Example_canvas_animatePath.vue';
+import App from './Example_pdf_table_test.vue';
 import pdfFrame from './components/index.js';
-// import pdfFrame from './../dist/pdf-frame.es.js';
 
 createApp(App).component("pdfFrame", pdfFrame).mount('#app');
