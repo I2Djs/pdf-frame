31f88166747bde7ca8201393deae1ab04053b52e
Narayana Swamy <narayanaswamy14@gmail.com>
2024-01-06 16:31:18 +0530

    updated Example

--- packages/pdf-frame-vue/src/main.js
+++ packages/pdf-frame-vue/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import './style.css';
-import App from './Example_pdf_table_test.vue';
+import App from './Example_pdf.vue';
 import pdfFrame from './components/index.js';
 
 createApp(App).component("pdfFrame", pdfFrame).mount('#app');
